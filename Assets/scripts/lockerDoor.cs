44579ec4b1ca4d91c470b6ec5865ba78f6fc35e0
tallbob99 <davidm1ller@shaw.ca>
2020-12-04 01:03:36 -0700

    messed with hinges and whatnot, no luck

---
+++ Assets/scripts/lockerDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockerDoor : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody.MovePosition(target.transform.position);
+       
+    }
+}
