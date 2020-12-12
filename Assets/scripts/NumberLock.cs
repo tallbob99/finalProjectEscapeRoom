ed122201ca3643e66d6d969aba9ac34d121c44c4
tallbob99 <davidm1ller@shaw.ca>
2020-12-11 17:40:46 -0700

    started work on lock code

---
+++ Assets/scripts/NumberLock.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberLock : MonoBehaviour
+{
+    public Text Number;
+    public Text correctNumber;
+    public bool numberCorrect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Number.text = "0";
+        correctNumber.text = "7";
+    }
+
+
+    public void ButtonToggle() 
+    
+    {
+      if (Number.text == "0") 
+       {
+            Number.text = "1";
+        }
+      else if (Number.text == "1")
+        {
+            Number.text = "2";
+        }
+
+      else if (Number.text == "2")
+        {
+            Number.text = "3";
+        }
+        else if (Number.text == "3")
+        {
+            Number.text = "4";
+        }
+        else if (Number.text == "4")
+        {
+            Number.text = "5";
+        }
+        else if (Number.text == "5")
+        {
+            Number.text = "6";
+        }
+        else if (Number.text == "6")
+        {
+            Number.text = "7";
+        }
+        else if (Number.text == "7")
+        {
+            Number.text = "8";
+        }
+        else if (Number.text == "8")
+        {
+            Number.text = "9";
+        }
+        else if (Number.text == "9")
+        {
+            Number.text = "0";
+        }
+
+        if (Number.text == correctNumber.text) 
+        {
+            Debug.Log("correct");
+        
+        }
+    }
+}
