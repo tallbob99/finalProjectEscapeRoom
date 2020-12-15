10bbbf6b1ab02d09c3ec74611ff461a5c1d41b09
tallbob99 <davidm1ller@shaw.ca>
2020-12-15 04:27:59 -0700

    box locks work

---
+++ Assets/scripts/boxLocks.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class boxLocks : MonoBehaviour
+{
+    public Text Number1;
+    public Text Number2;
+    public Text Number3;
+    public Text Number4;
+    public Text correctNumber1;
+    public Text correctNumber2;
+    public Text correctNumber3;
+    public Text correctNumber4;
+    public Collider interactableCollider;
+    public Collider staticCollider;
+    public GameObject codeLock;
+    public bool codesolved;
+    public GameObject keyLock;
+    public bool keySolved;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Number1.text == correctNumber1.text && Number2.text == correctNumber2.text && Number3.text == correctNumber3.text && Number4.text == correctNumber4.text)
+        {
+            codeLock.SetActive(false);
+            codesolved = true;
+        }
+
+        if (keySolved == true && codesolved == true) 
+        {
+            interactableCollider.enabled = true;
+            staticCollider.enabled = false;
+        }
+    {
+        
+    }
+
+}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.tag == "Key")
+        {
+            keyLock.SetActive(false);
+            keySolved = true;
+        }
+        
+    }
+}
