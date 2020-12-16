085b8b58e2eb44142b68714098ae04445669b7ff
tallbob99 <davidm1ller@shaw.ca>
2020-12-16 03:08:55 -0700

    watch beeps for five seconds when there are two minutes left

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 10;
+    public float timeRemaining = 180;
     public bool timerIsRunning = false;
     public Text timeText;
+    public AudioSource watchBeep;
+    public bool watchBeeping;
+    public bool twoMinutes = false;
 
     private void Start()
     {
@@ -17,12 +21,37 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
+        if (twoMinutes == false)
+        {
+            if (watchBeeping == false)
+            {
+                if (timeRemaining < 120)
+                {
+                    watchBeep.Play();
+                    watchBeeping = true;
+                    twoMinutes = true;
+                }
+            }
+        }
+        else
+        {
+            if (timeRemaining < 115)
+            {
+                watchBeep.Stop();
+                watchBeeping = false;
+            }
+
+        }
+        
+
         if (timerIsRunning)
         {
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
+                
+                
             }
             else
             {
@@ -31,6 +60,8 @@ public class Timer : MonoBehaviour
                 timerIsRunning = false;
             }
         }
+
+        
     }
 
     void DisplayTime(float timeToDisplay)
