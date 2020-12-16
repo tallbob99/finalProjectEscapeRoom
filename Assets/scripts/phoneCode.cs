b45d628806c03a86bcb5d50bfd663b0f6db67988
tallbob99 <davidm1ller@shaw.ca>
2020-12-16 05:52:59 -0700

    updated phone lock script

---
+++ Assets/scripts/phoneCode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class phoneCode : MonoBehaviour
+{
+    public Text Number1;
+    public Text Number2;
+    public Text Number3;
+    public Text Number4;
+    public Text correctNumber1;
+    public Text correctNumber2;
+    public Text correctNumber3;
+    public Text correctNumber4;
+    public GameObject endMenu1;
+    public GameObject timer;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Number1.text == correctNumber1.text && Number2.text == correctNumber2.text && Number3.text == correctNumber3.text && Number4.text == correctNumber4.text)
+        {
+
+
+            Debug.Log("code correct");
+            endMenu1.SetActive(true);
+            timer.SetActive(false);
+
+
+
+        }
+    }
+}
