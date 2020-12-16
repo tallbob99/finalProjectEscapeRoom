61248338faf558c626d926da7487e2ed95b5a355
tallbob99 <davidm1ller@shaw.ca>
2020-12-16 08:07:29 -0700

    codes now randomized

---
+++ Assets/scripts/textRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textRandomizer : MonoBehaviour
+{
+    float randomNumber;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomNumber = Random.Range(0, 10);
+        text.text = (randomNumber.ToString());
+    }
+
+  
+}
